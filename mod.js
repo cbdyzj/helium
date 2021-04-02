e28fc1fa628f2af10d9412fea18c6bdbec61af15
cbdyzj <cbdyzj@jianzhao.org>
2021-04-02 23:11:15 +0800

    Add .gitignore

--- mod.js
+++ mod.js
@@ -1,57 +1,75 @@
 function handleRequest(request) {
-  const { pathname } = new URL(request.url);
+    const { pathname } = new URL(request.url)
 
-  if (pathname.startsWith("/html")) {
-    const html = `<html>
-      <p><b>Message:</b> Hello from Deno Deploy.</p>
-      </html>`;
+    if (pathname.startsWith("/html")) {
+        const html = `
+            <!doctype html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport"
+                      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title>helium</title>
+            </head>
+            <body>
+            <main>
+                <p><b>Message:</b> Hello from Deno Deploy.</p>
+            </main>
+            </body>
+            </html>`
 
-    return new Response(html, {
-      headers: {
-        // The interpretation of the body of the response by the client depends
-        // on the 'content-type' header.
-        // The "text/html" part implies to the client that the content is HTML
-        // and the "charset=UTF-8" part implies to the client that the content
-        // is encoded using UTF-8.
-        "content-type": "text/html; charset=UTF-8",
-      },
-    });
-  }
+        return new Response(html, {
+            headers: {
+                "content-type": "text/html; charset=UTF-8",
+            },
+        })
+    }
 
-  if (pathname.startsWith("/json")) {
-    // Use stringify function to convert javascript object to JSON string.
-    const json = JSON.stringify({
-      message: "Hello from Deno Deploy",
-    });
+    if (pathname.startsWith("/json")) {
+        // Use stringify function to convert javascript object to JSON string.
+        const json = JSON.stringify({
+            message: "Hello from Deno Deploy",
+        })
 
-    return new Response(json, {
-      headers: {
-        "content-type": "application/json; charset=UTF-8",
-      },
-    });
-  }
+        return new Response(json, {
+            headers: {
+                "content-type": "application/json; charset=UTF-8",
+            },
+        })
+    }
 
-  return new Response(
-    `<body
-      align="center"
-      style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;"
-    >
-      <h1>Return JSON and/or HTML Example</h1>
-      <p>
-        <a href="/html">/html</a> - responds with HTML to the request.
-      </p>
-      <p>
-        <a href="/json">/json</a> - responds with JSON to the request.
-      </p>
-    </body>`,
-    {
-      headers: {
-        "content-type": "text/html; charset=UTF-8",
-      },
-    },
-  );
+    return new Response(`
+        <!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport"
+                  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>helium</title>
+        </head>
+        <body>
+        <main>
+            <h1>Return JSON and/or HTML Example</h1>
+            <p>
+                <a href="/html">/html</a> - responds with HTML to the request.
+            </p>
+            <p>
+                <a href="/json">/json</a> - responds with JSON to the request.
+            </p>
+        </main>
+        
+        </body>
+        </html>`,
+        {
+            headers: {
+                'content-type': 'text/html; charset=UTF-8',
+            },
+        },
+    )
 }
 
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
-});
+addEventListener('fetch', (event) => {
+    event.respondWith(handleRequest(event.request))
+})
